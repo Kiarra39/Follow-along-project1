e196ef6ffcecd2bb67b0fbcf8ace7c5b4e67071c
Kiesha <kiesha.maria@kalvium.community>
2024-12-17 22:03:24 +0530

    milestone 7 completed

--- ecom_backend/src/utils/sendmail.js
+++ ecom_backend/src/utils/sendmail.js
@@ -3,7 +3,7 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   auth: {
-    user: 'naayaankumar@gmail.com',
+    user: 'kieshagibin@gmail.com',
     pass: 'nuxl sjhn bmwy qfmu',
   },
 });
