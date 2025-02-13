7e935c36240e7a33d0cad2dc08e67d217de4f546
Kiesha Maria <kiesha.maria@kalvium.community>
2025-02-13 12:36:47 +0530

    milestone 28 completed

--- ecom_frontend/src/Pages/CartPage.jsx
+++ ecom_frontend/src/Pages/CartPage.jsx
@@ -1,16 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import CartCard from '../component/ProductCard/CartCard';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import CartCard from "../component/ProductCard/CartCard";
+import { Link } from "react-router-dom";
 
 function CardPage() {
   const [UsersCartData, setUsersCartData] = useState([]);
 
   useEffect(() => {
     const getCartData = async () => {
-      const token = localStorage.getItem('token');
+      const token = localStorage.getItem("token");
       if (!token) {
-        return alert('Token is missing , Please login');
+        return alert("Token is missing , Please login");
       }
       const response = await axios.get(
         `http://localhost:8080/cart/get-user-cart-data?token=${token}`
@@ -23,26 +23,35 @@ function CardPage() {
   }, []);
   return (
     <div>
-      <Link to={`/select-address`}>
-        <button className="bg-slate-800 text-white px-5 py-2 rounded-md ml-40">
-          Checkout
-        </button>
-      </Link>
-      {UsersCartData?.map((singleCartObject, index) => {
-        return (
-          <div key={index}>
-            <CartCard
-              title={singleCartObject.productId?.title}
-              image={singleCartObject.productId?.images[0]}
-              description={singleCartObject.productId?.description}
-              originalPrice={singleCartObject.productId?.originalPrice}
-              discountedPrice={singleCartObject.productId?.discountedPrice}
-              id={singleCartObject._id}
-              createdBy={'blah.com'}
-            />
-          </div>
-        );
-      })}
+      {UsersCartData.length > 0 ? (
+        <>
+          <Link to={`/select-address`}>
+            <button className="bg-slate-800 text-white px-5 py-2 rounded-md ml-40">
+              Checkout
+            </button>
+          </Link>
+          {UsersCartData?.map((singleCartObject, index) => {
+            return (
+              <div key={index}>
+                <CartCard
+                  title={singleCartObject.productId?.title}
+                  images={singleCartObject.productId?.images}
+                  //   Index={index}
+                  description={singleCartObject.productId?.description}
+                  originalPrice={singleCartObject.productId?.originalPrice}
+                  discountedPrice={singleCartObject.productId?.discountedPrice}
+                  id={singleCartObject.productId?._id}
+                  createdBy={"sarah@gmail.com"}
+                />
+              </div>
+            );
+          })}
+        </>
+      ) : (
+        <div className="flex justify-center max-h-[100vh] items-center">
+          <h1>Cart is empty</h1>
+        </div>
+      )}
     </div>
   );
 }
--- ecom_frontend/src/component/ProductCard/CartCard.jsx
+++ ecom_frontend/src/component/ProductCard/CartCard.jsx
@@ -1,5 +1,5 @@
-import { Link } from 'react-router-dom';
-import { X, ArrowDown, RotateCcw } from 'lucide-react';
+import { Link } from "react-router-dom";
+import { X, ArrowDown, RotateCcw } from "lucide-react";
 export default function CartCard({
   title,
   images,
@@ -11,10 +11,11 @@ export default function CartCard({
   id,
   createdBy,
   orderStatus,
+  handleCancel,
 }) {
   return (
     <div className="max-w-4xl mx-auto bg-white rounded-lg shadow p-4">
-       {orderStatus && (
+      {orderStatus && (
         <div className="bg-blue-100 text-blue-800 px-3 py-1 rounded-full text-sm w-[18%] text-center mb-5">
           {orderStatus}
         </div>
@@ -24,7 +25,7 @@ export default function CartCard({
         <div className="relative w-64">
           <Link to={`/product-details/${id}`}>
             <img
-               src={typeof images == 'string' ? images : images[0]}
+              src={typeof images == "string" ? images : null}
               alt="Product Image"
               className="rounded-lg object-cover"
             />
@@ -39,7 +40,10 @@ export default function CartCard({
               <p className="text-lg text-gray-600">{description}</p>
               <p className="text-gray-500 mt-1">Sold by: {createdBy}</p>
             </div>
-            <button className="text-gray-500 hover:text-gray-700">
+            <button
+              className="text-gray-500 hover:text-gray-700"
+              onClick={orderStatus ? () => handleCancel(id) : () => {}}
+            >
               <X className="w-6 h-6" />
             </button>
           </div>
